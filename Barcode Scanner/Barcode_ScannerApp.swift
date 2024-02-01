f1369e27b5ea80d7b06b06a0b7dbb780d22625f0
Yuri <yuriyuri200117@gmail.com>
2024-02-01 16:36:03 +0100

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Yuri Petrosyan on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

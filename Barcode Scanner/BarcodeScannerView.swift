279f98cd87fbe09d796ab875f77e0dd446864dde
Yuri <yuriyuri200117@gmail.com>
2024-02-04 17:18:10 +0100

    init

--- Barcode Scanner/BarcodeScannerView.swift
+++ Barcode Scanner/BarcodeScannerView.swift
@@ -2,15 +2,38 @@
 
 import SwiftUI
 
+
 struct BarcodeScannerView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        NavigationStack{
+            VStack{
+                
+                ScannerView().clipShape(
+                RoundedRectangle(cornerRadius: 19.0, style: .continuous))
+                    .frame(maxWidth: .infinity, maxHeight: 300)
+                    .padding(3)
+
+                Spacer().frame(height: 60)
+                
+                Label("Scanned Barcode", systemImage: "barcode.viewfinder")
+                    .font(.title)
+                    .fontWeight(.medium)
+               
+                
+                Text("Not Yet Scanned")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(.green)
+                
+                  
+            }
+            .navigationTitle("Barcode Scanner")
+            
         }
-        .padding()
+        
+        
+        
     }
 }
 
--- Barcode Scanner/ScannerView.swift
+++ Barcode Scanner/ScannerView.swift
@@ -7,9 +7,27 @@
 
 import SwiftUI
 
-struct ScannerView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct ScannerView: UIViewControllerRepresentable {
+   
+    
+    func makeUIViewController(context: Context) -> ScannerVC {
+        ScannerVC(scannerDelegate: context.coordinator)
+    }
+    
+    func updateUIViewController(_ uiViewController: ScannerVC, context: Context) { }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator()
+    }
+    
+    final class Coordinator: NSObject, ScannerVCDelegate {
+        func didFind(barcode: String) {
+            print(barcode)
+        }
+        
+        func didSurface(error: CameraError) {
+            print(error.rawValue)
+        }
     }
 }
 
